ca6008e9f3cd7c2c498e740838bdc7abfd85a7f8
MrCronkite <vladshima99@gmail.com>
2022-05-02 00:46:07 +0300

    first commit

---
+++ CloneBootleFLip/CloneBootleFLip/MenuScene.swift
@@ -0,0 +1,14 @@
+//
+//  MenuScene.swift
+//  CloneBootleFLip
+//
+//  Created by admin1 on 2.05.22.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+    override func didMove(to view: SKView) {
+        self.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    }
+}
